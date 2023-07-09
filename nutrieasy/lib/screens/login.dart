58e90f47877d12ab9ad93556ccdc8f70a93ab52f
keila <keila.viana@fastsolucoes.com.br>
2023-07-09 19:21:44 -0300

    [Refactory] Separando a login page do main.dart

---
+++ nutrieasy/lib/screens/login.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import '../main.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: 303,
+                child: TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    hintText: 'Insira seu email',
+                    hintStyle: TextStyle(
+                      color: Color(0xFF919191),
+                      fontSize: 12,
+                      fontFamily: 'Public Sans',
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 0.72,
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF528540),
+                        width: 0.50,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF528540),
+                        width: 0.50,
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.all(16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: 303,
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Insira sua senha',
+                    hintStyle: TextStyle(
+                      color: Color(0xFF919191),
+                      fontSize: 12,
+                      fontFamily: 'Public Sans',
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 0.72,
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF528540),
+                        width: 0.50,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF528540),
+                        width: 0.50,
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.all(16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Container(
+                width: 303,
+                height: 44,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                    side: const BorderSide(width: 1, color: Color(0xFF528540)),
+                  ),
+                  shadows: const [
+                    BoxShadow(
+                      color: Color(0x3F000000),
+                      blurRadius: 4,
+                      offset: Offset(0, 4),
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: const Color.fromRGBO(82, 133, 64, 1),
+                    minimumSize: const Size(303, 44),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      side: const BorderSide(
+                        color: Color.fromRGBO(82, 133, 64, 1),
+                        width: 1,
+                      ),
+                    ),
+                    elevation: 4,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SegundaRota(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Entrar',
+                    style: TextStyle(
+                      color: Color.fromRGBO(82, 133, 64, 1),
+                      fontFamily: 'Public Sans',
+                      fontSize: 16,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: 0.96,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Cadastro()),
+                  );
+                },
+                child: Container(
+                  width: 303,
+                  height: 44,
+                  decoration: ShapeDecoration(
+                    color: const Color(0xFF528540),
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                          width: 0.50, color: Color(0x444A4A4A)),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    shadows: const [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      ),
+                    ],
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Criar conta',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontFamily: 'Public Sans',
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.96,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
