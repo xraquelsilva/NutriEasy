cd1e576ebe07b465c52a30fa55d1e40339fa5f3e
Luis Felipe Coelho Garcia de Lima <lfcgl22@outlook.com>
2023-08-15 13:16:39 -0700

    Create header.dart

---
+++ nutrieasy/lib/screens/header.dart
@@ -0,0 +1,23 @@
+appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100),
+        child: AppBar(
+          elevation: 0,
+          leading: IconButton(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            alignment: Alignment.bottomLeft,
+            icon: Icon(Icons.arrow_back),
+            color: Colors.black54,
+            onPressed: () {         
+            },
+          ),
+
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Color(0xFFCDDE47),
+                  Colors.white,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ), ),),  ), ),
